df20110c4b7f6335263bd3e39384770abb8c2492
Prakyath Kantharaju <prakyathk33@gmail.com>
2024-10-14 21:45:32 -0500

    added arge paraser for the collect polar

--- scripts/collect_polar.py
+++ scripts/collect_polar.py
@@ -1,8 +1,7 @@
 import os
 import asyncio
 import yaml
-
-
+import argparse
 
 from HIL.cost_acquisition.polar.polar import Polar
 
@@ -13,17 +12,26 @@ def start_polar(address):
     Start the Polar data collection.
     """
     polar_inst=Polar(address)
-    # start the Polar data collection
-    # polar(polar_address)
     os.environ["PYTHONASYNCIODEBUG"] = str(1)
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(polar_inst.main())
 
 
+def run():
+    """
+    Run the Polar data collection with the given configuration.
+    """
+    parser = argparse.ArgumentParser(description="Polar data collection script")
+    parser.add_argument("--config", default="configs/polar.yml", help="Path to the configuration file")
+    parser.add_argument("--address", help="Polar device address (overrides config file)")
+    args = parser.parse_args()
+    with open(args.config, 'r') as file:
+        polar_information = yaml.safe_load(file)
+    
+    device_address = args.address or polar_information['address']
+    start_polar(device_address)
+
+
 if __name__ == "__main__":
-    file = open('configs/polar.yml', 'r')
-    polar_information = yaml.safe_load(file)
-    address = polar_information['address']
-    print(address)
-    start_polar(address)
+    run()
